05d41103c7edd73a3eec85a3eef37fe8c5320869
muupan <muupan@gmail.com>
2014-02-03 23:26:06 +0900

    Initialize neural_net.ann with NULL, avoiding freeing it in destroy()

--- src/include/fann_cpp.h
+++ src/include/fann_cpp.h
@@ -854,7 +854,7 @@ public:
 	    See also:
 	    		<copy_from_struct_fann>
         */
-	neural_net(const neural_net& other)
+	neural_net(const neural_net& other) : ann(NULL)
 	{
 	    copy_from_struct_fann(other.ann);
 	}
@@ -866,7 +866,7 @@ public:
 	   See also:
 	    		<copy_from_struct_fann>
         */
-	neural_net(struct fann* other)
+	neural_net(struct fann* other) : ann(NULL)
 	{
 	    copy_from_struct_fann(other);
 	}
